54b5d555af03f00a7ea01dd926d234d78164f839
Kiran Majeed <142150139+kiranmajeediss@users.noreply.github.com>
2026-06-12 12:15:07 +0000

    variable

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,21 @@
+const accountId= 12345
+let userName ="kiranmajeed"
+var userEmail ="kiranmajeed@gmail.com"
+
+cityName = "ahamadPur East"
+
+
+// accountId = 12  //for constant we can't change const variable value
+accountEmail ="kini@gmaill.comm"
+accountName="kini"
+cityName = "bwp"
+let accountState;
+
+/*
+prefer not to use the var keyword for variable declearation 
+because it occure the issue in the block scope and  function scope
+*/
+
+
+console.log(accountId);
+console.table([accountId,userName,userEmail,cityName,accountState])
