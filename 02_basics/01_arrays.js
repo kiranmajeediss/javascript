e635f0e6d89e50a6314ee6d685211ef3dab16a10
Kiran Majeed <142150139+kiranmajeediss@users.noreply.github.com>
2026-06-24 18:46:59 +0000

    array

---
+++ 02_basics/01_arrays.js
@@ -0,0 +1,44 @@
+/**Array: collection of items under a single vaiable name, */
+const myArray = [0,1,2,3,4,5,6,true,"kiran"]
+const myHeors = ["shaktiman", "naagraj"]
+const myArr2 = new Array(1, 2, 3, 4)
+// console.log(myArray[1]);
+// console.log(myArray)
+// console.log(myArr2)
+
+/**  ARRAY METHODS */
+const myArr = [0, 1, 2, 3, 4, 5]
+
+
+// myArr.push(6)
+// myArr.push(7)
+// myArr.pop()
+// console.log(myArr.inlcude)
+// myArr.unshift(9)
+
+// myArr.shift()
+// console.log(myArr);
+// ....
+// console.log(myArr.includes(9));
+// console.log(myArr.indexOf(3));
+
+// const newArr = myArr.join()
+
+// console.log(myArr);
+// console.log( newArr);
+
+// slice, splice
+
+console.log("A ", myArr);
+
+const myn1 = myArr.slice(1, 3)
+
+console.log(myn1);
+console.log("B ", myArr);
+
+
+const myn2 = myArr.splice(1, 3)
+console.log("C ", myArr);
+console.log(myn2);
+
+
