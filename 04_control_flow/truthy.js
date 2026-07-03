dd782634f7e8193d3b419125c21a74efb4436c77
Kiran Majeed <142150139+kiranmajeediss@users.noreply.github.com>
2026-07-03 12:02:58 +0000

    truthly

---
+++ 04_control_flow/truthy.js
@@ -0,0 +1,29 @@
+const userEmail = []
+
+if (userEmail) {
+    console.log("Got user email");
+} else {
+    console.log("Don't have user email");
+}
+
+// falsy values
+
+// false, 0, -0, BigInt 0n, "", null, undefined, NaN
+/**
+ * undefined:Variable exists but has no value yet
+ * null: Intentionally empty / "nothing here"
+ * 
+ */
+
+//truthy values
+// "0", 'false', " ", [], {}, function(){}
+
+// if (userEmail.length === 0) {
+//     console.log("Array is empty");
+// }
+
+const emptyObj = {}
+
+if (Object.keys(emptyObj).length === 0) {
+    console.log("Object is empty");
+}
