63d2cc9cd0c7ba04a549f922e76d28e818a8faa0
Kiran Majeed <142150139+kiranmajeediss@users.noreply.github.com>
2026-07-06 11:51:27 +0000

    while , do while

---
+++ 05_iteration/two.js
@@ -0,0 +1,33 @@
+/**
+ * while: Check FIRST, then run
+ * do...while: Run FIRST, then check
+ */
+//=========while loop===============
+let num =10;
+while(num<20){
+    // console.log(`your number is ${num}`)
+    num ++;
+}
+let index = 0
+while (index <= 10) {
+//   console.log(`Value of index is ${index}`);
+   index = index + 2
+ }
+
+//  while in array
+let myArray = ["apple","orange","grapes"]
+let arr =0;
+while(arr < myArray.length){
+    // console.log(`Fruite  is ${myArray[arr]}`);
+    arr++;
+    
+}
+//=========do while loop===============
+let score = 11
+
+do {
+    // console.log(`Score is ${score}`);
+    score++
+} 
+while (score <= 10);
+
